29cffb63bb3547d09830e86bfff9c5aa4355a267
LVOL98 <lukas98@live.dk>
2021-02-09 14:40:26 +0100

    Setup of sqlite connection done, and example in printing ids, still needs a reference to /tmp

--- minitwit.go
+++ minitwit.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	// db
+	_ "github.com/mattn/go-sqlite3"
+	"database/sql"
+	
+	// misc
+	"fmt"
+)
+
+var (
+	database *sql.DB	
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	database, _ = sql.Open("sqlite3", "minitwit.db") // TODO: make it reference /tmp
+	defer database.Close()
+	
+	row, _ := database.Query("select user_id from user")
+	defer row.Close()
+	for row.Next() {
+		var id int
+		row.Scan(&id)
+		fmt.Println(id)
+	}	
 }
