a14eb3bc22684e15900b5e5ad24578d78c0361a0
LVOL98 <lukas98@live.dk>
2021-02-09 14:28:03 +0100

    Setup of timeline using mux done

--- minitwit.go
+++ minitwit.go
@@ -6,8 +6,33 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var (
+	username string
+)
+
+// Route: /
+// Method: GET
+func timeline(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+// Route: /public
+// Method: GET
+func public_timeline(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
+// Route: /<username> 
+// Method: GET
+func user_timeline(w http.ResponseWriter, r *http.Request) {
+	// TODO
+}
+
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/", timeline)
+	router.HandleFunc("/public", public_timeline)
+	router.HandleFunc("/" + username, user_timeline)
 	
 	http.Handle("/", router)
 	
