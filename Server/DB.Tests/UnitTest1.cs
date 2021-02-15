cd4356c584e0f8993ba1bdaef6529c4c6e57b319
LVOL98 <lukas98@live.dk>
2021-02-15 15:54:38 +0100

    Added test for DB

---
+++ Server/DB.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Minitwit.Entities;
+
+namespace DB.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+          var test = new Follower();
+        }
+    }
+}
