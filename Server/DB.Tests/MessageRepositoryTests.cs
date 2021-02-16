ca0de77f30da1a2b991d020313b2b7a9b2bf7b7f
LVOL98 <lukas98@live.dk>
2021-02-16 08:24:34 +0100

    Started testing of message repo, will finish at another time

--- Server/DB.Tests/MessageRepositoryTests.cs
+++ Server/DB.Tests/MessageRepositoryTests.cs
@@ -1,7 +1,8 @@
 using System;
 using Xunit;
-using Minitwit.Entities;
 using Repos;
+using System.Linq;
+using Minitwit.Entities;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,8 +17,8 @@ namespace DB.Tests
 
         public MessageRepositoryTests()
         {
-            // _connection = new SqliteConnection("DataSource=:memory:");
-            // _connection.Open();
+            _connection = new SqliteConnection("Filename=:memory");
+            _connection.Open();
             var builder = new DbContextOptionsBuilder<MinitwitContext>().UseSqlite(_connection);
             _context = new DBTestContext(builder.Options);
             _context.Database.EnsureCreated();
@@ -30,6 +31,21 @@ namespace DB.Tests
             var listOfMessages = _repo.GetAllMessageFromUser(1);
 
             Assert.NotEmpty(listOfMessages);
+        }
+
+        [Fact]
+        public void Add_1_message_to_empty_DB_returns_list_size_2()
+        {
+            //Given
+            var message = new Message{ author_ID = 1, text = "This is the message" };
+        
+            //When
+            _repo.AddMessage(message);
+
+            var listOfMessages = _repo.GetAllMessageFromUser(1);
+        
+            //Then
+            Assert.Equal(2, listOfMessages.ToList().Count);
         }
     }
 }
