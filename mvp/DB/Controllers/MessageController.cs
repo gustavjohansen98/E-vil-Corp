033ba0efa78e6472d98d037162e9a80f9bd51809
LVOL98 <lukas98@live.dk>
2021-02-17 10:01:26 +0100

    Incompatible

--- mvp/DB/Controllers/MessageController.cs
+++ mvp/DB/Controllers/MessageController.cs
@@ -21,51 +21,51 @@ namespace Server.Controllers
             _repoUser = repoUser;
         }
 
-        [HttpGet]
-        public ActionResult<IEnumerable<Message>> GetAllMessages()
-        {
-            // TODO: not_req-from_simulator
+        // [HttpGet]
+        // public ActionResult<IEnumerable<Message>> GetAllMessages()
+        // {
+        //     // TODO: not_req-from_simulator
             
-            var users = _repoUser.GetAllUsers();
+        //     var users = _repoUser.GetAllUsers();
             
-            var messages = (from m in _repoMessage.GetAllMessages().ToList()
-                           where m.flagged == 0
-                           orderby m.pub_date
-                           // Filtered messages
-                           select new 
-                           { 
-                               content = m.text,
-                               pub_date = m.pub_date,
-                               user = users.Where(u => u.user_id == m.author_id).Select(u => u.username)
-                           }).Take(LIMIT);
+        //     var messages = (from m in _repoMessage.GetAllMessages().ToList()
+        //                    where m.flagged == 0
+        //                    orderby m.pub_date
+        //                    // Filtered messages
+        //                    select new 
+        //                    { 
+        //                        content = m.text,
+        //                        pub_date = m.pub_date,
+        //                        user = users.Where(u => u.user_id == m.author_id).Select(u => u.username)
+        //                    }).Take(LIMIT);
             
-            return Ok(messages);            
-        }
+        //     return Ok(messages);            
+        // }
 
-        [HttpGet("{username}")]
-        public ActionResult<IEnumerable<Message>> GetMessagesFromAGivenUser(string username)
-        {
-            // TODO update latest
+        // [HttpGet("{username}")]
+        // public ActionResult<IEnumerable<Message>> GetMessagesFromAGivenUser(string username)
+        // {
+        //     // TODO update latest
 
-            // TODO: not re_from_reposonse
+        //     // TODO: not re_from_reposonse
 
-            var user_id = _repoUser.GetUserIDFromUsername(username);
+        //     var user_id = _repoUser.GetUserIDFromUsername(username);
 
-            if (user_id != -1) return NotFound();
+        //     if (user_id != -1) return NotFound();
 
-            var messages = (from m in _repoMessage.GetAllMessages()
-                           where m.flagged == 0 &&
-                                 m.author_id == user_id // user_id?
-                           orderby m.pub_date
-                           select new 
-                           {
-                               content = m.text,
-                               pub_date = m.pub_date,
-                               user = username
-                           }).Take(LIMIT);
+        //     var messages = (from m in _repoMessage.GetAllMessages()
+        //                    where m.flagged == 0 &&
+        //                          m.author_id == user_id // user_id?
+        //                    orderby m.pub_date
+        //                    select new 
+        //                    {
+        //                        content = m.text,
+        //                        pub_date = m.pub_date,
+        //                        user = username
+        //                    }).Take(LIMIT);
 
-            return Ok(messages);
-            throw new NotImplementedException();
-        }
+        //     return Ok(messages);
+        //     throw new NotImplementedException();
+        // }
     }   
 }
