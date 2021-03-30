8498f525bea1e0eedcc858b7a86ef6be90434cad
luka <luka@itu.dk>
2021-03-30 09:07:27 +0200

    Done testing SHA256 hasher

--- mvp/EvilClient.Tests/ViewModels/UtilViewModelTests.cs
+++ mvp/EvilClient.Tests/ViewModels/UtilViewModelTests.cs
@@ -20,14 +20,27 @@ namespace EvilClient.Tests
         }
 
         [Fact]
-        public void Given_password_hashed_MD5_hasher_return_the_same()
+        public void Given_password_hashed_stringToHash_with_algorithm_return_the_same()
         {
             //Given
             var passwordHashedMD5 = "5f4dcc3b5aa765d61d8327deb882cf99";
-        
+
             //When
             var actualMD5Hashed = util.stringToHash(password, MD5.Create());
-        
+
+            //Then
+            Assert.Equal(passwordHashedMD5, actualMD5Hashed);
+        }
+
+        [Fact]
+        public void Given_password_hashed_stringToHash_with_SHA256_return_the_same()
+        {
+            //Given
+            var passwordHashedMD5 = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
+
+            //When
+            var actualMD5Hashed = util.stringToHash(password, SHA256.Create());
+
             //Then
             Assert.Equal(passwordHashedMD5, actualMD5Hashed);
         }
