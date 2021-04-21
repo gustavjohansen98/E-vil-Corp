e417efd766710d6a23997f1a1c5af4d5da06bded
abea <abea@itu.dk>
2021-04-21 17:05:26 +0200

    Email Validation Implemented and user already exists error added

--- EvilTwitter/EvilClient/ViewModels/AuthenticationCallApi.cs
+++ EvilTwitter/EvilClient/ViewModels/AuthenticationCallApi.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Minitwit.Entities;
 using Newtonsoft.Json;
@@ -59,6 +62,12 @@ namespace EvilClient.ViewModels
 
         public async Task<HttpStatusCode> SignUp(string username, string email, string password)
         {
+            var validationOfEmail = ValidateEmail(email);
+            if (!validationOfEmail)
+            {
+                return BadRequest;//UnprocessableEntity;
+            } 
+
             var userToDB = new User
             {
                 username = username,
@@ -78,5 +87,15 @@ namespace EvilClient.ViewModels
 
             return BadRequest;
         }
+
+        public bool ValidateEmail(string email)
+        {
+            //Got from stackoverflow: https://stackoverflow.com/questions/201323/how-to-validate-an-email-address-using-a-regular-expression
+            var pattern = @"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*)@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])";
+            var reg = new Regex(pattern);
+            
+            return reg.IsMatch(email); 
+        }
+
     }
 }
