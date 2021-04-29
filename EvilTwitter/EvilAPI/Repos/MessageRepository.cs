79e89d09659433cbfab542c1c55053aff68b89f6
gujo <gujo@itu.dk>
2021-04-29 16:29:10 +0200

    deployment script now deletes old docker images, and message timelines are correctly ordered and much faster

--- EvilTwitter/EvilAPI/Repos/MessageRepository.cs
+++ EvilTwitter/EvilAPI/Repos/MessageRepository.cs
@@ -55,7 +55,7 @@ namespace EvilAPI.Repos
                     where m.flagged == 0 &&
                     m.author_id == u.user_id &&
                     u.user_id == user_id
-                    orderby m.pub_date descending
+                    orderby m.message_id descending
                     select new UserMessageDTO
                     {
                         username = u.username,
@@ -67,12 +67,15 @@ namespace EvilAPI.Repos
 
         public IEnumerable<UserMessageDTO> GetAllMessages()
         {
+
+            // 2021-04-15 14:20:47.5910938
+
             TickTock.Inc();
             return (from m in _context.Message
                     from u in _context.User
                     where m.flagged == 0 &&
                     m.author_id == u.user_id
-                    orderby m.pub_date descending
+                    orderby m.message_id descending
                     select new UserMessageDTO
                     {
                         username = u.username,
@@ -81,6 +84,7 @@ namespace EvilAPI.Repos
                         pub_date = m.pub_date
                     }).Take(LIMIT);
         }
+
 
         public IEnumerable<UserMessageDTO> GetOwnAndFollowedMessages(int user_id)
         {
@@ -94,7 +98,7 @@ namespace EvilAPI.Repos
                         where f.who_id == user_id
                         select f.whom_id).Contains(u.user_id)
                    )
-                   orderby m.pub_date descending
+                   orderby m.message_id descending
                    select new UserMessageDTO
                    {
                         username = u.username,
